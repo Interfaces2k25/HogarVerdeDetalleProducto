a51264a670874830a911e22c6412a12594d33baa
Jessica <jesbromac@alu.edu.gva.es>
2025-11-20 19:10:19 +0100

    inicio en el centro del nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="w-full bg-[rgba(212,218,178,1)] text-black p-4 shadow-lg flex justify-center gap-8">
+    <nav className="w-full bg-[rgba(212,218,178,1)] text-black p-4 shadow-lg flex flex-col justify-center gap-8 text-center">
       <img
         className="w-30 h-auto flex flex-col items-center justify-center mx-auto"
         src="imagenes/LogoSinFondoHogarVerde.png"
